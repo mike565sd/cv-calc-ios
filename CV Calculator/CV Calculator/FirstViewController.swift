3e1ff1693d597f5d9d4fe92a5ced1e543e386c67
Mike Weaver <mike565sd@gmail.com>
2014-10-23 11:13:01 -0700

    Changed from page-based to tab-based

--- CV Calculator/CV Calculator/FirstViewController.swift
+++ CV Calculator/CV Calculator/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  DataViewController.swift
+//  FirstViewController.swift
 //  CV Calculator
 //
 //  Created by Michael Weaver on 10/23/14.
@@ -8,11 +8,7 @@
 
 import UIKit
 
-class DataViewController: UIViewController {
-
-    @IBOutlet weak var dataLabel: UILabel!
-    var dataObject: AnyObject?
-
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +18,6 @@ class DataViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        if let obj: AnyObject = dataObject {
-            self.dataLabel!.text = obj.description
-        } else {
-            self.dataLabel!.text = ""
-        }
     }
 
 
---
+++ CV Calculator/CV Calculator/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CV Calculator
+//
+//  Created by Michael Weaver on 10/23/14.
+//  Copyright (c) 2014 Bürkert. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
